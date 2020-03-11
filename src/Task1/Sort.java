11c2d57d2f61b94d7618a765d1ca4ae8cf609457
RossiV1712 <48483291+RossiV1712@users.noreply.github.com>
2020-03-11 16:30:46 +0000

    AddedShuttleSort

--- src/Task1/Sort.java
+++ src/Task1/Sort.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class Sort {
     int[] StoredNum;
 
-    public Sort(int[] InputArray) {
+    public Sort(int[] InputArray) { // The constructor to create the Sort object
         StoredNum = InputArray;
     }
 
@@ -25,29 +25,20 @@ public class Sort {
     }
 
     public void quickSort() {
-        int total = StoredNum.length;
-        int temp = 0;
+        int total = StoredNum.length; // Store the length of the array
+        int temp = 0; // Create and set the temp variable to 0 initially
 
-        for (int i = 0; i < total - 1; i++){
-                if (StoredNum[i] > StoredNum[i + 1]) {
-                    int j = i - 1;
-
-                    temp = StoredNum[i + 1];
-                    StoredNum[i + 1] = StoredNum[i];
-//                    System.out.println(temp);
-//                    System.out.println(StoredNum[0] + "," + StoredNum[1] + "," + StoredNum[2] + "," + StoredNum[3] + "," + StoredNum[4] + "," + StoredNum[5] + "," + StoredNum[6]);
-
-                    while (j >= 0 && StoredNum[j] > temp) {
-//                        System.out.println(j);
-                        StoredNum[j + 1] = StoredNum[j];
-//                        System.out.println(temp);
-//                        System.out.println(StoredNum[0] + "," + StoredNum[1] + "," + StoredNum[2] + "," + StoredNum[3] + "," + StoredNum[4] + "," + StoredNum[5] + "," + StoredNum[6]);
-                        j--;
+        for (int i = 0; i < total - 1; i++){ // For each between 0 and 1 less than the length of the array.
+                if (StoredNum[i] > StoredNum[i + 1]) { // If the selected number is larger than that on the right
+                    int j = i - 1; // Set j to check the next on the left
+                    temp = StoredNum[i + 1]; // Store the smaller (on the right) number in a temporary variable
+                    StoredNum[i + 1] = StoredNum[i]; // Set the larger number to one position to the right (in the place of the smaller number)
+                    while (j >= 0 && StoredNum[j] > temp) { // While J is 0 or larger and the checked number is larger than the stored one
+                        StoredNum[j + 1] = StoredNum[j]; // Set the larger number to one position to the right (in the place of the smaller number)
+                        j--; // Minus one from j to allow the loop to end and iterate through the required variables
                     }
-                    StoredNum[j + 1] = temp;
-                    System.out.println("End of Pass: " + StoredNum[0] + "," + StoredNum[1] + "," + StoredNum[2] + "," + StoredNum[3] + "," + StoredNum[4] + "," + StoredNum[5] + "," + StoredNum[6]);
+                    StoredNum[j + 1] = temp; // Set the value from the temporary variable to its new position
         }
-
         }
     }
 
